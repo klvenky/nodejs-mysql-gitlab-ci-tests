a4ad8c053e3682c4c1e4663e0da1974f4d56e26c
venkatesh <venkatesh@convertcart.com>
2020-09-17 11:14:03 +0530

    comented mysql service in gitlab ci. Added sample test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // Create the basic express app here.
 
 // Connect to the test_database here
+  
--- app.test.js
+++ app.test.js
@@ -1,8 +1,17 @@
+function add(a, b) {
+  return a + b;
+}
+
 describe("Unit Tests run", function () {
-  beforeAll() {
-  // initialize the server & db here
-  }
-  afterAll() {
-  // close the app server & db here
-  }
+  beforeAll(function () {
+    // initialize the server & db here
+    console.log("in before all");
+  });
+  afterAll(function () {
+    // close the app server & db here
+    console.log("in after all");
+  });
+  test("sample test", function () {
+    expect(add(1, 2)).toEqual(3);
+  });
 });
