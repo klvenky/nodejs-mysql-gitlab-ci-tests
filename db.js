0a4b3332cca9cb6f7b1517a47881d96f686ab841
venkatesh <venkatesh@convertcart.com>
2020-09-24 15:33:49 +0530

    alternate connections for mysql in ci/local

--- db.js
+++ db.js
@@ -5,9 +5,12 @@ let db = {};
 let close;
 let initDone = false;
 
+// Variable will be available in test mode
+const isCITest = !!process.env.CI_JOB_STAGE;
+console.log("---", isCITest);
 const init = async () => {
   conn = await initMysqlConn(
-    "mysql://root:root@localhost:3306/test_database",
+    `mysql://root:root@${isCITest ? "mysql" : "localhost"}:3306/test_database`,
     "./schema.sql"
   );
   db.employee = new Employee(conn);
